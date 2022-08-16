ff6097ed26198ad43966d4b3f261bb368198c8fd
Vitaliy Vasiliev <93582413+Ierixorn@users.noreply.github.com>
2022-08-16 17:55:04 +0300

    Модальное окно для Tablet и исправления

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,4 @@
 import './slider.js';
 import './slider-pagination.js';
 import './сards.js';
+import './request-window.js';
---
+++ source/js/request-window.js
@@ -0,0 +1,27 @@
+const requestWindow = document.querySelector('.site-body__modal-wrapper');
+const closeButton = document.querySelector('.request-form__close-button');
+
+const removeClass = () => {
+  requestWindow.classList.remove('site-body__modal-wrapper--opened');
+}
+
+const closeWindow = () => {
+  requestWindow.classList.remove('site-body__modal-wrapper--fade-in')
+  requestWindow.classList.add('site-body__modal-wrapper--fade-out');
+  setTimeout(removeClass, 2000)
+  requestWindow.classList.add('site-body__modal-wrapper--closed');
+  closeButton.removeEventListener('click', closeWindow);
+}
+
+document.body.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  if (evt.target.dataset.request === "open") {
+    requestWindow.classList.remove('site-body__modal-wrapper--fade-out')
+
+    requestWindow.classList.add('site-body__modal-wrapper--opened');
+    requestWindow.classList.add('site-body__modal-wrapper--fade-in')
+
+    closeButton.addEventListener('click', closeWindow);
+  }
+})
